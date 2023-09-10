6f1fd9206d79ce2f72a59df34741bf20c8e9a845
Mykhailo Lozinskyi <mykhailo.lozinskyi.pz.2021@lpnu.ua>
2023-09-10 15:05:47 +0300

    Implement the simplest version of the algorithm - move one cell along the X axis and along the Y axis

---
+++ LozinskyiMykhailo.RobotChallenge/LozinskyiMykhailo.RobotChallenge/LozinskyiMykhailoAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using Robot.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LozinskyiMykhailo.RobotChallenge
+{
+    public class LozinskyiMykhailoAlgorithm : IRobotAlgorithm
+    {
+        public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            var myRobot = robots[robotToMoveIndex];
+
+            var newPosition = myRobot.Position;
+            newPosition.X = newPosition.X + 1;
+            newPosition.Y = newPosition.Y + 1;
+
+            return new MoveCommand() { NewPosition = newPosition };
+        }
+
+        public string Author { 
+            get { return "Lozinskyi Mykhailo"; }
+        }
+
+        public string Description { 
+            get { return "Demo for students"; } 
+        }
+    }
+}
