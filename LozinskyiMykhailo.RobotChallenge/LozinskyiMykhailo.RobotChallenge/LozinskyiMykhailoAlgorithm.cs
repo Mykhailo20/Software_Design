b2d3f236ea3fbbdbc4f07a4837e4a811d80a73fa
Mykhailo Lozinskyi <mykhailo.lozinskyi.pz.2021@lpnu.ua>
2023-09-10 15:58:25 +0300

    Implement a more complex version of the algorithm, which searches for the nearest station (code from the manual)

--- LozinskyiMykhailo.RobotChallenge/LozinskyiMykhailo.RobotChallenge/LozinskyiMykhailoAlgorithm.cs
+++ LozinskyiMykhailo.RobotChallenge/LozinskyiMykhailo.RobotChallenge/LozinskyiMykhailoAlgorithm.cs
@@ -9,15 +9,67 @@ namespace LozinskyiMykhailo.RobotChallenge
 {
     public class LozinskyiMykhailoAlgorithm : IRobotAlgorithm
     {
+        public Position FindNearestFreeStation(Robot.Common.Robot movingRobot, Map map, IList<Robot.Common.Robot> robots)
+        {
+            EnergyStation nearest = null;
+            int minDistance = int.MaxValue;
+            foreach (var station in map.Stations)
+            {
+                if (IsStationFree(station, movingRobot, robots))
+                {
+                    int d = DistanceHelper.FindDistance(station.Position, movingRobot.Position);
+                    if (d < minDistance)
+                    {
+                        minDistance = d;
+                        nearest = station;
+                    }
+                }
+            }
+            return nearest == null ? null : nearest.Position;
+        }
+
+        public bool IsStationFree(EnergyStation station, Robot.Common.Robot movingRobot, IList<Robot.Common.Robot> robots)
+        {
+            return IsCellFree(station.Position, movingRobot, robots);
+        }
+
+        public bool IsCellFree(Position cell, Robot.Common.Robot movingRobot, IList<Robot.Common.Robot> robots)
+        {
+            foreach (var robot in robots)
+            {
+                if (robot != movingRobot)
+                {
+                    if (robot.Position == cell)
+                        return false;
+                }
+            }
+            return true;
+        }
+
         public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
         {
-            var myRobot = robots[robotToMoveIndex];
+            Robot.Common.Robot movingRobot = robots[robotToMoveIndex];
+            if ((movingRobot.Energy > 500) && (robots.Count < map.Stations.Count))
+            {
+                return new CreateNewRobotCommand();
+            }
 
-            var newPosition = myRobot.Position;
-            newPosition.X = newPosition.X + 1;
-            newPosition.Y = newPosition.Y + 1;
+            Position stationPosition = FindNearestFreeStation(robots[robotToMoveIndex], map, robots);
 
-            return new MoveCommand() { NewPosition = newPosition };
+
+            if (stationPosition == null)
+            {
+                return null;
+            }
+                
+            if (stationPosition == movingRobot.Position)
+            {
+                return new CollectEnergyCommand();
+            }
+            else
+            {
+                return new MoveCommand() { NewPosition = stationPosition };
+            }
         }
 
         public string Author { 
