4a38ff8860b9d0035b8794b170aa1dbcfa3f0b8b
Mykhailo Lozinskyi <mykhailo.lozinskyi.pz.2021@lpnu.ua>
2023-10-31 21:11:28 +0200

    Lab5: Implement a more advanced version of ImagePathConverter and provide caching

--- Lab5/Code/ComputerCoursesSystem.Model/ComputerCoursesSystem.UI/Convertors/ImagePathConverter.cs
+++ Lab5/Code/ComputerCoursesSystem.Model/ComputerCoursesSystem.UI/Convertors/ImagePathConverter.cs
@@ -12,11 +12,17 @@ namespace ComputerCoursesSystem.UI.Convertors
 {
     public class ImagePathConverter : IValueConverter
     {
+        Dictionary<TeachingStyle, BitmapImage> cache = new Dictionary<TeachingStyle, BitmapImage>();
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var teachingStyle = (TeachingStyle)value;
-            var uri = new Uri($"../Images/TeacherStyle/{teachingStyle}_Style.png", UriKind.Relative);
-            return new BitmapImage(uri);
+            if(!cache.ContainsKey(teachingStyle))
+            {
+                var uri = new Uri($"../Images/{parameter}/{teachingStyle}.png", UriKind.Relative);
+                cache.Add(teachingStyle, new BitmapImage(uri));
+            }
+            
+            return cache[teachingStyle];
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
