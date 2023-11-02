fe53b52114a11122bcd49d7d7682a8a7ca281a27
Mykhailo Lozinskyi <mykhailo.lozinskyi.pz.2021@lpnu.ua>
2023-11-02 18:35:29 +0200

    Lab5: Implement a simple animation for the transition buttons between tables using the DoubleAnimation class

--- Lab5/Code/ComputerCoursesSystem.Model/ComputerCoursesSystem.UI/Views/MainWindow.xaml.cs
+++ Lab5/Code/ComputerCoursesSystem.Model/ComputerCoursesSystem.UI/Views/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,12 +25,25 @@ namespace ComputerCoursesSystem.UI
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        /*private void buttonLectureBased_Click(object sender, RoutedEventArgs e)
-        {
-            var teacher = (TeacherViewModel)dataGridTeachers.SelectedItem;
-            teacher.Style = Model.TeachingStyle.LectureBased;
-        }*/
+            DoubleAnimation buttonImageAnimationTeacher = new DoubleAnimation()
+            {
+                From = teachersImage.ActualWidth, To = 100, Duration = TimeSpan.FromSeconds(3)
+            };
+            teachersImage.BeginAnimation(WidthProperty, buttonImageAnimationTeacher);
+
+            DoubleAnimation buttonImageAnimationCourse = new DoubleAnimation() { 
+                From = coursesImage.ActualWidth, To = 100, Duration = TimeSpan.FromSeconds(3)
+            };
+            coursesImage.BeginAnimation(WidthProperty, buttonImageAnimationCourse);
+
+            DoubleAnimation buttonImageAnimationAssignment = new DoubleAnimation()
+            {
+                From = assignmentsImage.ActualWidth,
+                To = 100,
+                Duration = TimeSpan.FromSeconds(3)
+            };
+            assignmentsImage.BeginAnimation(WidthProperty, buttonImageAnimationAssignment);
+        }
     }
 }
