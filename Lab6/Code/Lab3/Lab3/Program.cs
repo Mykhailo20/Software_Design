bf1ece0ad52dd6b0cbfefd4315b034a848d6c2f5
Mykhailo Lozinskyi <mykhailo.lozinskyi.pz.2021@lpnu.ua>
2023-11-14 12:18:05 +0200

    Lab6: Implement adding a new record to the "person" table

--- Lab6/Code/Lab3/Lab3/Program.cs
+++ Lab6/Code/Lab3/Lab3/Program.cs
@@ -3,6 +3,7 @@ using Lab3.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,12 +15,47 @@ namespace Lab3
         {
             using(var db = new ComputerCoursesDbContext())
             {
-                var persons = db.Persons.ToList();
-                foreach (var person in persons)
-                {
-                    Console.WriteLine($"{person.PersonId} {person.FirstName} {person.LastName} {person.Email}");
-                }
+                InsertPerson(db);
+                ShowPersons(db);
+            }
+        }
 
+        private static void ShowPersons(ComputerCoursesDbContext db)
+        {
+            var persons = db.Persons.ToList();
+            foreach (var person in persons)
+            {
+                Console.WriteLine($"{person.PersonId} {person.FirstName} {person.LastName} {person.Email}");
+            }
+        }
+
+        private static void InsertPerson(ComputerCoursesDbContext db)
+        {
+            // Create a new Person instance with the data you want to insert
+            Person newPerson = new Person
+            {
+                FirstName = "Ben",
+                LastName = "Ten",
+                BirthDate = new DateOnly(2005, 5, 15),
+                Username = "ben10",
+                Password = GetHashedPassword("ben10_password"),
+                Email = "ben.ten@gmail.com",
+                Phone = "0960001009"
+            };
+
+            // Add the new person to the context and save changes
+            db.Persons.Add(newPerson);
+            Console.WriteLine("\nNew record in table \"person\" was successfully added!\n");
+            db.SaveChanges();
+        }
+
+        // Helper method to hash the password using SHA-256
+        private static string GetHashedPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
             }
         }
     }
