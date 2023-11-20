a1ed3805f983aa40e6ae21fecfaabfa9246599af
Mykhailo Lozinskyi <mykhailo.lozinskyi.pz.2021@lpnu.ua>
2023-11-20 19:00:14 +0200

    Lab7: Implement the simplest version of TeacherController

---
+++ Lab7/Code/Lab4/Lab4/Controllers/TeacherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeacherController: ControllerBase
+    {
+        public static Teacher teacher = new Teacher();
+
+        [HttpGet]
+        public ActionResult<Teacher> Get()
+        {
+            return Ok(teacher);
+        }
+    }
+}
---
+++ Lab7/Code/Lab4/Lab4/Models/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Lab4.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string FirstName { get; set; } = "Ardit";
+        public string LastName { get; set; } = "Pythoner";
+        public string MiddleName { get; set; } = string.Empty;
+        public DateOnly BirthDate { get; set; } = new DateOnly();
+
+        public TeachingStyle Style { get; set; } = TeachingStyle.Mentorship;
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TeachingStyle
+    {
+        Mentorship = 1,
+        LectureBased = 2,
+        ProjectBased = 3
+    }
+}
