16d7f67d3a5524f4a094fed19f4b6ef83a66595f
Mykhailo Lozinskyi <mykhailo.lozinskyi.pz.2021@lpnu.ua>
2023-12-09 11:15:17 +0200

    Lab8: Implement routing for teacher-table and skill-table components

--- Lab8/angular-kpz-mykhailo/src/app/app-routing.module.ts
+++ Lab8/angular-kpz-mykhailo/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeacherTableComponent } from './components/teacher-table/teacher-table.component';
+import { SkillTableComponent } from './components/skill-table/skill-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'teachers', component: TeacherTableComponent },
+  { path: 'skills', component: SkillTableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [TeacherTableComponent, SkillTableComponent]
--- Lab8/angular-kpz-mykhailo/src/app/app.module.ts
+++ Lab8/angular-kpz-mykhailo/src/app/app.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TeacherTableComponent } from './components/teacher-table/teacher-table.component';
 import { TeacherService } from './services/teacher.service';
 import { TeachingStylePipe } from './pipes/teaching-style.pipe';
 import { BlueBackgroundDirective } from './directives/blue-background.directive';
 import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { headerInterceptor } from './interceptors/header.interceptor';
-import { SkillTableComponent } from './components/skill-table/skill-table.component';
 import { SkillService } from './services/skill.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeacherTableComponent,
+    routingComponents,
     TeachingStylePipe,
     BlueBackgroundDirective,
-    SkillTableComponent
   ],
   imports: [
     BrowserModule,
